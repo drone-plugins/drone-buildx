bce0885acfbcd62a6baffcd6528a313a9f4ab539
Jamie Li <tianyi.li@harness.io>
2025-11-05 16:11:28 -0800

    [CI-19689]: enhance buildx to automatically pass all aws variables to buildkit

--- app.go
+++ app.go
@@ -460,7 +460,7 @@ func Run() {
 			Usage:  "path to save Docker image as tar file",
 			EnvVar: "PLUGIN_TAR_PATH, PLUGIN_DESTINATION_TAR_PATH",
 		},
-		cli.StringFlag{
+		cli.BoolFlag{
 			Name:   "buildkit-inherit-auth",
 			Usage:  "inherit auth from docker daemon",
 			EnvVar: "PLUGIN_BUILDKIT_INHERIT_AUTH",
